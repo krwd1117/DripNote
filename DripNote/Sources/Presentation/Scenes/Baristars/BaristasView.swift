d553febe7cfc74a4cf2e84f742e0911ecf6f8f3c
김정완 <krwd1117@gmail.com>
2025-04-07 12:06:38 +0900

    fix: SplashScreen 의 아이콘 제거

--- DripNote/Sources/Presentation/Scenes/Baristars/BaristasView.swift
+++ DripNote/Sources/Presentation/Scenes/Baristars/BaristasView.swift
@@ -74,42 +74,48 @@ fileprivate struct BaristasGridView: View {
         GridItem(.flexible(), spacing: 16)
     ]
     
-    private var items: [GridItemType] {
-        var result: [GridItemType] = []
-        for (index, recipe) in self.viewModel.recipes.enumerated() {
-            result.append(.recipe(recipe))
-            if (index + 1) % 3 == 0 {
-                result.append(.ad(UUID()))
-            }
-        }
-        return result
-    }
+//    private var items: [GridItemType] {
+//        var result: [GridItemType] = []
+//        for (index, recipe) in self.viewModel.recipes.enumerated() {
+//            result.append(.recipe(recipe))
+//            if (index + 1) % 3 == 0 {
+//                result.append(.ad(UUID()))
+//            }
+//        }
+//        return result
+//    }
     
     var body: some View {
         ScrollView {
             LazyVGrid(columns: columns, spacing: 16) {
-                ForEach(items) { item in
-                    switch item {
-                    case .recipe(let recipe):
-                        Button {
-                            coordinator.push(.detail(recipe))
-                        } label: {
-                            BaristasGridCell(recipe: recipe)
-                        }
-                        .frame(maxWidth: .infinity, alignment: .center)
-                        
-                    case .ad:
-                        VStack {
-                            NativeAdContainerView(
-                                unitID: .baristas_grid_cell_unit_id,
-                                backgroundColor: Color.Custom.secondaryBackground.color
-                            )
-                            .frame(maxWidth: .infinity, alignment: .center)
-                        }
-                        .cornerRadius(16)
-                        .shadow(color: Color.Custom.darkBrown.color.opacity(0.05), radius: 4, x: 0, y: 2)
-                        .gridCellColumns(2)
+                ForEach(viewModel.recipes) { item in
+                    Button {
+                        coordinator.push(.detail(item))
+                    } label: {
+                        BaristasGridCell(recipe: item)
                     }
+                    .frame(maxWidth: .infinity, alignment: .center)
+//                    switch item {
+//                    case .recipe(let recipe):
+//                        Button {
+//                            coordinator.push(.detail(recipe))
+//                        } label: {
+//                            BaristasGridCell(recipe: recipe)
+//                        }
+//                        .frame(maxWidth: .infinity, alignment: .center)
+//                        
+//                    case .ad:
+//                        VStack {
+//                            NativeAdContainerView(
+//                                unitID: .baristas_grid_cell_unit_id,
+//                                backgroundColor: Color.Custom.secondaryBackground.color
+//                            )
+//                            .frame(maxWidth: .infinity, alignment: .center)
+//                        }
+//                        .cornerRadius(16)
+//                        .shadow(color: Color.Custom.darkBrown.color.opacity(0.05), radius: 4, x: 0, y: 2)
+//                        .gridCellColumns(2)
+//                    }
                 }
             }
             .padding(16)
--- DripNote/Sources/Presentation/Scenes/Splash/SplashView.swift
+++ DripNote/Sources/Presentation/Scenes/Splash/SplashView.swift
@@ -12,15 +12,15 @@ public struct SplashView: View {
         ZStack {
             Color.Custom.secondaryBackground.color.ignoresSafeArea()
             
-            VStack {
-                Spacer()
-                Image("AppIconImage")
-                    .resizable()
-                    .frame(width: 256, height: 256)
-                Spacer()
-            }
-            .frame(maxHeight: .infinity)
-            .ignoresSafeArea()
+//            VStack {
+//                Spacer()
+//                Image("AppIconImage")
+//                    .resizable()
+//                    .frame(width: 256, height: 256)
+//                Spacer()
+//            }
+//            .frame(maxHeight: .infinity)
+//            .ignoresSafeArea()
         }
         .task {
             withAnimation {
