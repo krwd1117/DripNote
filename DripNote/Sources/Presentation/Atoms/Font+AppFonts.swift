aac34e3f17dbb1f682e04daae103d9d0ef308059
krwd1117 <krwd1117@gmail.com>
2026-02-12 20:19:52 +0900

    feat(ui): Start Atomic Design refactor on new branch 'atomic-design-clean'

--- DripNote/Sources/Presentation/Atoms/Font+AppFonts.swift
+++
@@ -1,9 +0,0 @@
-import SwiftUI
-
-extension Font {
-    static let appTitle = Font.system(size: 28, weight: .bold)
-    static let appHeadline = Font.system(size: 22, weight: .semibold)
-    static let appBody = Font.system(size: 17, weight: .regular)
-    static let appCaption = Font.system(size: 13, weight: .light)
-    static let appAccent = Font.system(size: 17, weight: .medium, design: .rounded)
-}
